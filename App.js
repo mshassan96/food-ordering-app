f866c3cca8b07d305dad66821d28ed3de9bab437
Salman <miansalmanhassan@yahoo.com>
2024-08-24 05:23:10 +0500

    Basic React Running

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Content = () => <div>Test CONTENT</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Content />);
