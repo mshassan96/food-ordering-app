d75037887ce620ff54f7c452663d7780a9a97530
Salman <miansalmanhassan@yahoo.com>
2024-08-29 00:43:48 +0500

    Added state and filtering

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { restaurantsList } from "../utils/mockData";
+import { restaurantsList as list } from "../utils/mockData";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">Search</div>
-    <div className="restaurant-container">
-      {restaurantsList.map((restaurant) => (
-        <RestaurantCard key={restaurant.id} resData={restaurant} />
-      ))}
+const Body = () => {
+  const [restaurantsList, setRestaurantsList] = useState(list);
+  console.log({ restaurantsList, setRestaurantsList });
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredData = restaurantsList.filter(
+              (restaurant) => restaurant.rating > 4.8
+            );
+
+            setRestaurantsList(filteredData);
+          }}
+        >
+          Filter Top Rated Restaurants
+        </button>
+      </div>
+      <div className="restaurant-container">
+        {restaurantsList?.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Body;
